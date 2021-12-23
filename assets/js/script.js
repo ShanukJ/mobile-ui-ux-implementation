a105277835660837cf36919a54b1b9be06d542c0
Athindu <athinduama@gmail.com>
2021-12-24 00:24:38 +0530

    nav-bar & header template

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#category").hover(function() {
+		$(this).css("color", "black");
+	}, 
+	function() {
+		$(this).hide();
+		//$(this).css("display", "none");
+	});
+
+	
+});
