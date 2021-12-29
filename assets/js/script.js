1554a709b3d60048724b723c3ca44a24fc128c46
Jayamal Sanuka Hettiarachchi <jayashanuka@gmail.com>
2021-12-29 12:39:48 +0530

    Added script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,18 @@ $(document).ready(function() {
 		//$(this).css("display", "none");
 	});
 
+	$("#header").click(function(){
+		alert("Logo Header Clicked")
+    //$(".logo_header").hide();
+  });
+
+	$("#home").click(function(){
+		//alert("Icon Header Clicked")
+    //$(".logo_header").hide();
+		$( '#home_icon' ).removeClass( 'material-icons-outlined' );
+		$( '#home_icon' ).addClass( 'material-icons' );
+		//console.log(this)
+		alert(this.id);
+  });
 	
 });
